5c156676a1261755a9839bf49447455020b279f3
Jennah Yasin <89948730+jennahyasin@users.noreply.github.com>
2023-03-13 04:04:15 -0700

    Created student controller (add student, add hold, delete hold) and tested it.

--- cst438_register/src/main/java/com/cst438/controller/StudentController.java
+++ cst438_register/src/main/java/com/cst438/controller/StudentController.java
@@ -1,5 +1,96 @@
 package com.cst438.controller;
 
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import com.cst438.domain.StudentRepository;
+import com.cst438.domain.Student;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:3000", "https://registerf-cst438.herokuapp.com/"})
 public class StudentController {
+	@Autowired
+	StudentRepository studentRepository;
+	
+	/*
+	 * adding a new student to the system
+	 */
+	//TODO: make sure only admins can do this 
+	@PostMapping("/student/add/{email}/{name}")
+	@Transactional
+	public Student addNewStudent(@PathVariable String email,  @PathVariable String name)
+	{
+		Student newStudent = studentRepository.findByEmail(email);
+		if(newStudent != null) {
+			throw  new ResponseStatusException( HttpStatus.BAD_REQUEST, "Student with this email address already exists in the system: " + email);
+		}
+		else {
+			newStudent = new Student();
+			newStudent.setName(name);
+			newStudent.setEmail(email);
+			
+			studentRepository.save(newStudent);
+			
+			return newStudent;
+		}
+	}
+	
+	/*
+	 * Adding hold on student registration
+	 * Status Codes: 0 - No Hold
+	 * 				 1 - On Hold
+	 */
+	@PostMapping("/student/addhold/{email}")
+	@Transactional
+	public Student addHoldtoStudent(@PathVariable String email)
+	{
+		Student student = studentRepository.findByEmail(email);		
+		if(student == null) {
+			throw new ResponseStatusException( HttpStatus.BAD_REQUEST, "Student does not exist/not registered: " + email);
+		}
+		else {
+			student.setStatusCode(1);
+			student.setStatus("ON HOLD");
+			studentRepository.save(student);
+			return student;
+		}
+	}
+	
+	/*
+	 * Deleting (updating) hold on student registration
+	 * Status Codes: 0 - No Hold
+	 * 				 1 - On Hold
+	 */
+	@PutMapping("/student/deletehold/{email}")
+	@Transactional
+	public Student deleteHold(@PathVariable String email) {
+		
+		Student student = studentRepository.findByEmail(email);
+		if(student == null) {
+			throw new ResponseStatusException( HttpStatus.BAD_REQUEST,"Student is not registered: " + email);
+		}
+		String currentStatus = student.getStatus();
+		
+		if(currentStatus == null)
+		{
+			throw new ResponseStatusException( HttpStatus.BAD_REQUEST, "Student not on hold: " + email);
+		}
+		else {
+			student.setStatusCode(0);
+			student.setStatus(null);
+			studentRepository.save(student);
+			return student;
+		}
+	}
 
 }
+
+
---
+++ cst438_register/src/test/java/com/cst438/JunitTestStudent.java
@@ -0,0 +1,81 @@
+package com.cst438;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+
+
+import com.cst438.controller.StudentController;
+import com.cst438.domain.ScheduleDTO;
+import com.cst438.domain.Student;
+import com.cst438.domain.StudentRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ContextConfiguration(classes = { StudentController.class })
+@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest
+class JunitTestStudent {
+
+	static final String URL = "http://localhost:8080";
+	public static final String TEST_STUDENT_EMAIL = "123";
+	public static final String TEST_STUDENT_NAME  = "test";
+	
+	@MockBean
+	StudentRepository studentRepository;
+	
+
+	@Autowired
+	private MockMvc mvc;
+	
+	@Test
+	void addNewStudent() throws Exception {
+		MockHttpServletResponse response;
+		
+		Student student = new Student();
+		student.setName(TEST_STUDENT_NAME);
+		student.setEmail(TEST_STUDENT_NAME);
+
+		
+		given(studentRepository.findByEmail(TEST_STUDENT_EMAIL)).willReturn(null);
+		given(studentRepository.save(any(Student.class))).willReturn(student);
+		
+		
+		response = mvc.perform(
+				MockMvcRequestBuilders
+				.post("/student/add/{email}/{name}", TEST_STUDENT_EMAIL, TEST_STUDENT_NAME))
+				.andReturn().getResponse();
+				
+		Student student2 = fromJsonString(response.getContentAsString(), Student.class);
+		System.out.println(student2);
+		assertEquals(200, response.getStatus());
+		assertEquals(TEST_STUDENT_EMAIL, student2.getEmail());
+		
+		verify(studentRepository).save(any(Student.class));
+
+		//verify(studentRepository, times(1)).findByEmail(TEST_STUDENT_EMAIL);
+		}
+
+	private static <T> T  fromJsonString(String str, Class<T> valueType ) {
+		try {
+			return new ObjectMapper().readValue(str, valueType);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
