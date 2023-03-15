46752d19d059ed2e3f00bdebd63acb622bc63957
Jennah Yasin <89948730+jennahyasin@users.noreply.github.com>
2023-03-14 21:41:40 -0700

    Update StudentDTO.java
    Added comments for final commit of StudentDTO.java class.

--- cst438_register/src/main/java/com/cst438/domain/StudentDTO.java
+++ cst438_register/src/main/java/com/cst438/domain/StudentDTO.java
@@ -1,6 +1,8 @@
 package com.cst438.domain;
 
+//Data transfer object class for Student.java 
 public class StudentDTO {
+	//Public variables from Student.java class
 	public int student_id;
 	public String name;
 	public String email;
