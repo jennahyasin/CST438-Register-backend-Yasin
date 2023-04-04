354ea2c18d703e6d38d5f00287659bd7fd729bdc
Jennah Yasin <89948730+jennahyasin@users.noreply.github.com>
2023-04-04 13:51:44 -0700

    Update EndToEndStudentTest.java
    Added some comments to code for clarification

--- cst438_register/src/test/java/com/cst438/EndToEndStudentTest.java
+++ cst438_register/src/test/java/com/cst438/EndToEndStudentTest.java
@@ -20,14 +20,13 @@ import com.cst438.domain.StudentRepository;
 @SpringBootTest
 public class EndToEndStudentTest {
 
+	//Test Data 
 	public static final String CHROME_DRIVER_FILE_LOCATION = "/Users/jennahyasin/Downloads/chromedriver_mac64/chromedriver";
 	
 	public static final String URL = "http://localhost:3000";
 	
-//	public static final String TEST_STUDENT_EMAIL = "e2etest@csumb.edu";
 	public static final String TEST_STUDENT_EMAIL = "e2etest@csumb.edu";
 
-	
 	public static final String TEST_STUDENT_NAME = "e2e Test";
 	
 	public static final int SLEEP_DURATION = 1000;
@@ -50,26 +49,31 @@ public class EndToEndStudentTest {
 			
 			driver.get(URL);
 			Thread.sleep(SLEEP_DURATION);
+			//Selects the last semester choice 
 			WebElement we = driver.findElement(By.xpath("(//input[@type='radio'])[last()]"));
 			we.click();
  
+			//Clicks the add student button 
 			driver.findElement(By.xpath("//a[last()]")).click();
 			Thread.sleep(SLEEP_DURATION);
 			
-
-			
+			//Enters in the test data in the input fields (email, name)
 			driver.findElement(By.xpath("//input[@name='email']")).sendKeys(TEST_STUDENT_EMAIL);
 			Thread.sleep(SLEEP_DURATION);
 			driver.findElement(By.xpath("//input[@name='name']")).sendKeys(TEST_STUDENT_NAME);
 			Thread.sleep(SLEEP_DURATION);
+			//Clicks the Add Student button
 			driver.findElement(By.xpath("//button[@id='submitButton']")).click();
 			Thread.sleep(SLEEP_DURATION);
 			
+			//finds the added student in the repository
 			Student addedStudent = studentRepository.findByEmail(TEST_STUDENT_EMAIL);
+			//verifies that the added student isn't empty
 			assertNotNull(addedStudent, "Test student not added to database.");
 		} catch (Exception ex) {
 			throw ex;
 		} 
+		//close browser
 		finally {
 			driver.close();
 			driver.quit();
